67498f14b178fddf027ba71c0278616a066d5e04
beabear <2222030298@qq.com>
2022-12-27 00:22:27 +0800

    day 09

--- src/Connection.cpp
+++ src/Connection.cpp
@@ -1,18 +1,19 @@
 #include "Connection.h"
 #include "Socket.h"
 #include "Channel.h"
+#include "util.h"
+#include "Buffer.h"
 #include <unistd.h>
 #include <string.h>
-#include <stdio.h>
-#include <errno.h>
-#define READ_BUFFER 1024
+#include <iostream>
 
-Connection::Connection(EventLoop *_loop, Socket *_sock) : loop(_loop), sock(_sock), channel(nullptr)
+Connection::Connection(EventLoop *_loop, Socket *_sock) : loop(_loop), sock(_sock), channel(nullptr), inBuffer(new std::string()), readBuffer(nullptr)
 {
     channel = new Channel(loop, sock->getFd());
     std::function<void()> cb = std::bind(&Connection::echo, this, sock->getFd());
     channel->setCallback(cb);
     channel->enableReading();
+    readBuffer = new Buffer();
 }
 
 Connection::~Connection()
@@ -23,15 +24,14 @@ Connection::~Connection()
 
 void Connection::echo(int sockfd)
 {
-    char buf[READ_BUFFER];
+    char buf[1024];
     while (true)
     { //由于使用非阻塞IO，读取客户端buffer，一次读取buf大小数据，直到全部读取完毕
         bzero(&buf, sizeof(buf));
         ssize_t bytes_read = read(sockfd, buf, sizeof(buf));
         if (bytes_read > 0)
         {
-            printf("message from client fd %d: %s\n", sockfd, buf);
-            write(sockfd, buf, sizeof(buf));
+            readBuffer->append(buf, bytes_read);
         }
         else if (bytes_read == -1 && errno == EINTR)
         { //客户端正常中断、继续读取
@@ -40,7 +40,10 @@ void Connection::echo(int sockfd)
         }
         else if (bytes_read == -1 && ((errno == EAGAIN) || (errno == EWOULDBLOCK)))
         { //非阻塞IO，这个条件表示数据全部读取完毕
-            printf("finish reading once, errno: %d\n", errno);
+            printf("finish reading once\n");
+            printf("message from client fd %d: %s\n", sockfd, readBuffer->c_str());
+            errif(write(sockfd, readBuffer->c_str(), readBuffer->size()) == -1, "socket write error");
+            readBuffer->clear();
             break;
         }
         else if (bytes_read == 0)
